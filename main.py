78c2881ae17926963deccbe73ea56944ee3a9cfd
sososo345 <zh.gadzevich@mail.ru>
2025-03-20 16:37:20 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 import asyncio
+import random
 
 bot = Bot(token="7255704116:AAFWY64huuMSXOm4wOknzgLoxsEFbnlIeQE")
 dp = Dispatcher()
@@ -16,10 +17,27 @@ async def echo(message:types.message):
           await message.answer(text)
      else:
           await message.answer("напиши после /echo")
+
+          
 @dp.message(Command("photobobra"))
 async def cmd_img(message: types.Message):
      await message.answer_photo("https://i.pinimg.com/originals/88/b8/39/88b8396bc196fad7042a18c4e98875a1.jpg")
- 
+
+
+@dp.message(Command('/rps'))
+async def rps(message:types.message):
+     print(2344)
+     botschoose=["камень","ножницы","бумага"]
+     user = message.text.replace("/rps","").strip()
+     bot = random.choice(botschoose)
+     if (bot==user):
+          await message.answer("ничья")
+     elif(user=="камень"and bot=='ножницы') or (user=="ножницы"and bot=='бумага') or (user=="бумага"and bot=='камень'):
+          await message.answer(f"вы выбрали {user} бот выбрал {bot} вы выйграли")
+     else:
+          await message.answer(f"вы выбрали {user} бот выбрал {bot} вы проиграли")
+     
+
 async def main():
     print('Запкск бота')
     await dp.start_polling(bot)
